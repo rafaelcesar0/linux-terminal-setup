09d56af8448e9b1de28a799f3da0c4358fd96749
rafaelcesar0 <rafaelcesar0@proton.me>
2024-11-28 23:08:49 -0300

    feat: upd all v2.0

--- config/.zshrc
+++ config/.zshrc
@@ -1,30 +1,55 @@
-# If you come from bash you might have to change your $PATH.
+# Path configuration
 export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 
-# Path to your Oh My Zsh installation.
+# Oh My Zsh configuration
 export ZSH="$HOME/.oh-my-zsh"
-
+ZSH_THEME="robbyrussell"
 # export GIT_PAGER=
 
-# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="robbyrussell"
-
-# Uncomment one of the following lines to change the auto-update behavior
+# Oh My Zsh update configuration
 # zstyle ':omz:update' mode disabled  # disable automatic updates
 zstyle ':omz:update' mode auto      # update automatically without asking
 # zstyle ':omz:update' mode reminder  # just remind me to update when it's time
 
-plugins=(git colored-man-pages zsh-autosuggestions F-Sy-H web-search npm aliases)
+# Performance improvement for completions
+autoload -Uz compinit
+if [[ -n ~/.zcompdump(#qN.mh+24) ]]; then
+  compinit
+else
+  compinit -C
+fi
+
+# Oh My Zsh plugins
+plugins=(
+  git
+  colored-man-pages
+  zsh-autosuggestions
+  F-Sy-H
+  web-search
+  npm
+  aliases
+)
+
+# Add zsh-completions to fpath
 fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
 
+# Initialize tools
 source $ZSH/oh-my-zsh.sh
-
-# If you want to start Starship discontent the line below.
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 eval "$(starship init zsh)"
 eval "$(zoxide init zsh)"
-# User configuration
 
-# ~~~~~~~~~~~~~~~~~~~~ Aliases ~~~~~~~~~~~~~~~~~~~~
+export FZF_DEFAULT_OPTS='
+  --height 40%
+  --layout=reverse
+  --border
+  --info=inline
+  --prompt="❯ "
+  --pointer="❯"
+  --marker="❯"
+'
+
+# Aliases
 alias i='sudo apt install'
 alias i-get='sudo apt-get install'
 alias upd='sudo apt update && sudo apt upgrade -y'
--- setup.sh
+++ setup.sh
@@ -1,46 +1,85 @@
 #!/bin/zsh
 
-# Atualizando o Linux
-sudo apt update && sudo apt upgrade -y
-
-# Instalando plugins básicos do Oh-My-Zsh
-git clone https://github.com/z-shell/F-Sy-H.git \
-  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/F-Sy-H &&
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-
-# Obtendo o diretório onde o script está localizado
+# Obtendo o diretório onde o script está localizado (deve vir antes de usar $SCRIPT_DIR)
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-# # ~~~~~~~~~~~~~~~~~~~~ Fonts ~~~~~~~~~~~~~~~~~~~~
-# mkdir -p ~/.fonts
-# cp -f "$SCRIPT_DIR/fonts/*" ~/.fonts/
+# Função para verificar se comando foi executado com sucesso
+check_execution() {
+    if [ $? -eq 0 ]; then
+        echo "✓ $1 instalado com sucesso"
+    else
+        echo "✗ Erro ao instalar $1"
+        exit 1
+    fi
+}
 
-# ~~~~~~~~~~~~~~~~~~~~ zshrc  ~~~~~~~~~~~~~~~~~~~~
-cp -f "$SCRIPT_DIR/config/.zshrc" ~/
+echo "Iniciando configuração do ambiente..."
 
-# ~~~~~~~~~~~~~~~~~~~~ Starship ~~~~~~~~~~~~~~~~~~~~
+# Atualizando o Linux
+echo "Atualizando o sistema..."
+sudo apt update && sudo apt upgrade -y
+check_execution "Atualização do sistema"
+
+# Oh-My-Zsh Plugins
+echo "Instalando plugins do Oh-My-Zsh..."
+git clone https://github.com/z-shell/F-Sy-H.git \
+    ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/F-Sy-H
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+    ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions \
+    ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+check_execution "Plugins Oh-My-Zsh"
+
+# Starship
+echo "Instalando Starship..."
 curl -sS https://starship.rs/install.sh | sh
-# ~~~~~~~~~~~~~~~~~~~~ Starship.config ~~~~~~~~~~~~~~~~~~~~
+check_execution "Starship"
+
+# Starship Config
+echo "Configurando Starship..."
 mkdir -p ~/.config
 cp -f "$SCRIPT_DIR/config/starship.toml" ~/.config/
+check_execution "Configuração Starship"
 
-# ~~~~~~~~~~~~~~~~~~~~ Zoxide ~~~~~~~~~~~~~~~~~~~~
+# Zoxide
+echo "Instalando Zoxide..."
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+check_execution "Zoxide"
 
-#  ~~~~~~~~~~~~~~~~~~~~ NVM & Node ~~~~~~~~~~~~~~~~~~~~
+# Fzf
+echo "Instalando Fzf..."
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install -y
+check_execution "Fzf"
+
+# zshrc
+echo "Configurando zshrc..."
+cp -f "$SCRIPT_DIR/config/.zshrc" ~/
+check_execution "zshrc"
+
+# NVM & Node
+echo "Instalando NVM e Node..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-source ~/.zshrc
-nvm i --lts
-nvm use global default
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+check_execution "NVM"
 
-# ~~~~~~~~~~~~~~~~~~~~ Pnpm ~~~~~~~~~~~~~~~~~~~~
+echo "Instalando Node LTS..."
+nvm install --lts
+nvm use --lts
+nvm alias default 'lts/*'
+check_execution "Node LTS"
+
+# Pnpm
+echo "Instalando Pnpm..."
 curl -fsSL https://get.pnpm.io/install.sh | sh -
+check_execution "Pnpm"
 
-# ~~~~~~~~~~~~~~~~~~~~ Bun ~~~~~~~~~~~~~~~~~~~~
+# Bun
+echo "Instalando Bun..."
 sudo apt install -y unzip
 curl -fsSL https://bun.sh/install | bash
+check_execution "Bun"
 
-source ~/.zshrc
-
-echo "- Settings completed. Please restart the terminal to apply the changes."
+echo "✓ Configuração concluída! Por favor, reinicie o terminal para aplicar as mudanças."
